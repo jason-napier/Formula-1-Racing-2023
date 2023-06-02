1815c343134fe92a2090b16695237ccd2d4d964f
Lucas Ludwig <lucas.j.ludwig@gmail.com>
2023-06-01 20:02:35 -0400

    2nd update

--- Lucas/app.py
+++ Lucas/app.py
@@ -1,47 +1,26 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, json
 import requests
 
 app = Flask(__name__)
 
-# API base URL
-API_BASE_URL = "http://ergast.com/api/f1"
-
-# Home route
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-# Endpoint to fetch driver information
-@app.route('/api/drivers/<driver_id>')
-def get_driver_info(driver_id):
-    url = f"{API_BASE_URL}/drivers/{driver_id}.json"
-    response = requests.get(url)
+    response = requests.get('http://ergast.com/api/f1/2023/circuits.json')
     data = response.json()
-    return jsonify(data)
 
-# Endpoint to fetch constructor information
-@app.route('/api/constructors/<constructor_id>')
-def get_constructor_info(constructor_id):
-    url = f"{API_BASE_URL}/constructors/{constructor_id}.json"
-    response = requests.get(url)
-    data = response.json()
-    return jsonify(data)
+    circuits = []
+    for circuit in data['MRData']['CircuitTable']['Circuits']:
+        circuit_info = {
+            'circuitName': circuit['circuitName'],
+            'lat': float(circuit['Location']['lat']),
+            'long': float(circuit['Location']['long']),
+            'locality': circuit['Location']['locality'],
+            'country': circuit['Location']['country'],
+        }
+        circuits.append(circuit_info)
 
-# Endpoint to fetch race results
-@app.route('/api/races/<season>/<round>/results')
-def get_race_results(season, round):
-    url = f"{API_BASE_URL}/{season}/{round}/results.json"
-    response = requests.get(url)
-    data = response.json()
-    return jsonify(data)
+    return render_template('index.html', circuits=json.dumps(circuits))
 
-# Endpoint to fetch standings
-@app.route('/api/standings/<season>/<round>')
-def get_standings(season, round):
-    url = f"{API_BASE_URL}/{season}/{round}/driverStandings.json"
-    response = requests.get(url)
-    data = response.json()
-    return jsonify(data)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- Lucas/static/script.js
+++ Lucas/static/script.js
@@ -1,127 +1,39 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var select = document.getElementById('data-select');
-    select.addEventListener('change', fetchData);
-});
-
-function fetchData() {
-    var select = document.getElementById('data-select');
-    var selectedData = select.options[select.selectedIndex].value;
-
-    if (selectedData !== "") {
-        fetch(`/api/formula1/${selectedData}`)
-            .then(response => response.json())
-            .then(data => {
-                visualizeData(data, selectedData);
-            });
-    }
-}
-
-function visualizeData(data, selectedData) {
-    var visualizationDiv = document.getElementById('visualization');
-    visualizationDiv.innerHTML = '';
-
-    if (selectedData === 'drivers') {
-        visualizeDrivers(data);
-    } else if (selectedData === 'teams') {
-        visualizeTeams(data);
-    } else if (selectedData === 'manufacturers') {
-        visualizeManufacturers(data);
-    } else if (selectedData === 'races') {
-        visualizeRaces(data);
-    }
-}
-
-function visualizeDrivers(data) {
-    var drivers = data.map(driver => driver.name);
-    var points = data.map(driver => driver.points);
-
-    var ctx = document.createElement('canvas');
-    visualizationDiv.appendChild(ctx);
-
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: drivers,
-            datasets: [{
-                label: 'Points',
-                data: points,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-}
-
-function visualizeTeams(data) {
-    var teams = data.map(team => team.name);
-    var wins = data.map(team => team.wins);
-
-    var ctx = document.createElement('canvas');
-    visualizationDiv.appendChild(ctx);
-
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: teams,
-            datasets: [{
-                label: 'Wins',
-                data: wins,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-}
-
-function visualizeManufacturers(data) {
-    var manufacturers = data.map(manufacturer => ({
-        name: manufacturer.name,
-        country: manufacturer.country
-    }));
-
-    var ctx = document.createElement('canvas');
-    visualizationDiv.appendChild(ctx);
-
-    new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: manufacturers.map(manufacturer => manufacturer.name),
-            datasets: [{
-                label: 'Manufacturers',
-                data: manufacturers.map(manufacturer => 1),
-                backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)'],
-                borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)'],
-                borderWidth: 1
-            }]
-        },
-        options: {}
-    });
-}
-
-function visualizeRaces(data) {
-    var raceList = document.createElement('ul');
-
-    data.forEach(race => {
-        var raceItem = document.createElement('li');
-        raceItem.textContent = race.name;
-        raceList.appendChild(raceItem);
+// Fetch circuit data from the Flask API
+fetch('/api/circuits')
+  .then(response => response.json())
+  .then(data => {
+    const circuitMarkers = [];
+    const circuitIcon = L.icon({
+      iconUrl: 'static/images/circuit-marker.png',
+      iconSize: [25, 25],
+      iconAnchor: [12.5, 12.5],
     });
 
-    visualizationDiv.appendChild(raceList);
-}
+    data.forEach(circuit => {
+      const marker = L.marker([circuit.Location.lat, circuit.Location.long], { icon: circuitIcon })
+        .bindPopup(`<b>${circuit.circuitName}</b><br>${circuit.Location.locality}, ${circuit.Location.country}`)
+        .addTo(map);
+      circuitMarkers.push(marker);
+    });
+  })
+  .catch(error => console.log(error));
+
+// Fetch driver data from the Flask API
+fetch('/api/drivers')
+  .then(response => response.json())
+  .then(data => {
+    const driverMarkers = [];
+    const driverIcon = L.icon({
+      iconUrl: 'static/images/driver-marker.png',
+      iconSize: [25, 41],
+      iconAnchor: [12.5, 41],
+    });
+
+    data.forEach(driver => {
+      const marker = L.marker([driver.lat, driver.long], { icon: driverIcon })
+        .bindPopup(`<b>${driver.givenName} ${driver.familyName}</b><br>${driver.nationality}`)
+        .addTo(map);
+      driverMarkers.push(marker);
+    });
+  })
+  .catch(error => console.log(error));
